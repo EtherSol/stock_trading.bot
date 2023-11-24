c691b419a19a92e5ec09d9415cd86a5283a485e3
EtherSol <kylemz@arizona.edu>
2023-11-24 16:00:57 -0700

    schema

--- trading_bot.py
+++ trading_bot.py
@@ -21,18 +21,20 @@
 # IN: 30 min candles data (Close data)
 # OUT: UP/ DOWN / No Trend (strings)
 
-
-# LOOP
+#############
+# LOOP until timeout reached (ex. 30 min)
 # STEP 1: load 5 min candles
 # IN: asset (or whatever the API needs), time range+, candle size+
 # OUT: 5 min candles (OHLC for every candle)
 
 
 # STEP 2: perform instant trend analysis: confirm the trend dected by GT analysis
-# IN: 30 min candles data (Close data), output of the GT analysis (UP/ DOWN string)
+# IN: 5 min candles data (Close data), output of the GT analysis (UP/ DOWN string)
 # OUT: True (confirmed) / False (not confirmed)
 
 # STEP 3: perform RSI analysis
+# IN: 5 min candles data (Close data), output of the GT analysis (UP/ DOWN string)
+# OUT: True (confirmed) / False (not confirmed)
 # perform stochastic analysis
 
 
